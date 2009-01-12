7a2fbb52455269bf1b53ee154ab3b7361c12b9cb
AliRezaTaleghani <shayne@shayne-desktop.(none)>
2009-01-12 11:22:30 +0330

    AddDefaultUSer,Done

--- db/migrate/20090105075454_add_default_user.rb
+++ db/migrate/20090105075454_add_default_user.rb
@@ -1,7 +1,7 @@
 class AddDefaultUser < ActiveRecord::Migration
   def self.up
   	if !User.find_by_login('root')
-		User.create(:login => 'root', :email => 'root@localhost', :password => 'rootAdmin', :password_confirmation => 'rootAdmin')
+		User.create(:login => 'root', :email => 'root@localhost.com', :password => 'rootAdmin', :password_confirmation => 'rootAdmin')
 	end
   end
 
